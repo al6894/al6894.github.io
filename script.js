e8e42e2d55639cb5aaf0b074e21c89f93634e42f
al6894 <115506396+al6894@users.noreply.github.com>
2024-09-27 13:46:35 -0400

    added openQommute() script

---
+++ script.js
@@ -0,0 +1,4 @@
+function openQommute() {
+    window.open('https://github.com/andylee02/Qommute', '_blank');
+    window.open('/static/pdfs/qommute.pdf', '_blank');
+}
--- scripts.js
+++
@@ -1,4 +0,0 @@
-function openQommute() {
-    window.open('https://www.example1.com', '_blank');
-    window.open('https://www.example2.com', '_blank');
-}
